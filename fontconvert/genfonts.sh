46f9dbf005940ee539b02c7d7cf050e609f88390
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2019-05-05 13:51:37 -0700

    added custom font generator

---
+++ fontconvert/genfonts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Generate fonts for Adafruit-Gfx library usage
+
+# Generated font will be written in the same directory the script is invoked
+# Source fonts are expected in a subdirectory TTF
+
+# fonts="FreeSans ClearSans Hack"
+# styles="Regular Bold Italic"
+fonts="ClearSans"
+styles="Medium"
+sizes="9 12 18 24 36 48"
+
+for fo in $fonts
+do
+  for st in $styles
+  do  
+    for sz in $sizes
+    do
+      ttf=TTF/${fo}-${st}.ttf
+      inc=${fo}-${st}-${sz}pt7b.h
+      ../fontconvert/fontconvert ${ttf} ${sz} > ${inc}
+      echo "#include \"${inc}\""
+    done
+  done
+done
